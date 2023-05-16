61c426b19ab97714a019af84255a81edebbb5fe9
Justin Grote <JustinGrote@users.noreply.github.com>
2023-05-16 16:03:32 -0700

    Refactor WebSocketTransport to use a record for ClientWebSocket

--- src/WebSocketTransport.cs
+++ src/WebSocketTransport.cs
@@ -1,4 +1,5 @@
 using System.Management.Automation.Internal;
+using System.Management.Automation.Remoting;
 using System.Management.Automation.Remoting.Client;
 using System.Management.Automation.Runspaces;
 using System.Net.WebSockets;
@@ -6,21 +7,21 @@ using System.Text;
 
 namespace PoshTransports;
 
+public record WebSocketTarget(string Hostname, int Port, bool UseSSL)
+{
+  public string Protocol => UseSSL ? "wss" : "ws";
+  public Uri WebSocketUri => new($"{Protocol}://{Hostname}:{Port}/psrp");
+}
 /// <summary>
 /// Attaches to a TCP port where PS remoting messages are provided
 /// </summary>
 public class WebSocketConnectionInfo : UnauthenticatedRunspaceConnectionInfo
 {
-  internal readonly int Port;
-  internal readonly string Hostname;
-  internal Uri WebSocketUri;
+  internal readonly WebSocketTarget WebSocketTarget;
+  internal Uri WebSocketUri => WebSocketTarget.WebSocketUri;
   public WebSocketConnectionInfo(int port, string hostname = "localhost", bool useSSL = true)
   {
-    // We want a client at this point but we don't want to connect until the transport manager initiates it
-    Port = port;
-    Hostname = hostname;
-    string Protocol = useSSL ? "wss" : "ws";
-    WebSocketUri = new($"{Protocol}://{Hostname}:{Port}/psrp");
+    WebSocketTarget = new(hostname, port, useSSL);
   }
 
   public override BaseClientSessionTransportManager CreateClientSessionTransportManager(
@@ -36,16 +37,8 @@ public class WebSocketConnectionInfo : UnauthenticatedRunspaceConnectionInfo
 
   public override string ComputerName
   {
-    get => WebSocketUri.ToString();
+    get => WebSocketTarget.Hostname;
     set => throw new NotImplementedException("Cannot fetch Computername");
-  }
-
-  /// <summary>
-  /// Create shallow copy of NamedPipeInfo object.
-  /// </summary>
-  public override RunspaceConnectionInfo Clone()
-  {
-    return new WebSocketConnectionInfo(Port, Hostname);
   }
 }
 
@@ -66,15 +59,24 @@ class WebSocketTransportManager : SimpleTransportManagerBase
     WebSocketUri = webSocketUri;
   }
 
+  private Task? activeHandleDataTask;
   protected override void HandleDataFromClient(string data)
   {
     // We do not add a newline here, it will be added on the other side when passed to the named pipe, because websockets has the concept of messages and we don't need a delimiter, it'll simply signal the client when the bytes are finished writing
-    Client.SendAsync(
+
+    // We can only send one message at a time, so we need to wait for the previous message to finish sending before we can send the next one
+    // Ref: https://learn.microsoft.com/en-us/dotnet/api/system.net.websockets.websocket.sendasync?view=net-7.0
+    if (activeHandleDataTask is not null)
+    {
+      while (!activeHandleDataTask.Wait(500)) { }
+    }
+
+    activeHandleDataTask = Client.SendAsync(
       Encoding.UTF8.GetBytes(data),
       WebSocketMessageType.Text,
       true,
       default
-    ).GetAwaiter().GetResult();
+    );
   }
 
   protected override async Task<string> ReceiveDataFromTransport()
@@ -86,13 +88,28 @@ class WebSocketTransportManager : SimpleTransportManagerBase
     do
     {
       byte[] buffer = new byte[8194];
-
       receiveResult = await Client.ReceiveAsync(buffer, default);
       receiveStream.Write(buffer, 0, receiveResult.Count);
     } while (!receiveResult.EndOfMessage);
+
     // Rewind the memorystream so it can be read by readline
     receiveStream.Position = 0;
-    return reader.ReadLine() ?? throw new InvalidDataException("Received null data from websocket, this should never happen.");
+    var message = await reader.ReadLineAsync();
+
+    string errMessage = $"Websocket Server sent a close status of {receiveResult.CloseStatus} with reason {receiveResult.CloseStatusDescription}";
+
+    return message ?? throw receiveResult.CloseStatus switch
+    {
+      WebSocketCloseStatus.NormalClosure => new PSRemotingTransportException("Server sent a NormalClosure, these are initiated from the client so this should never happen"),
+      WebSocketCloseStatus.Empty => new PSRemotingTransportException("Received null data from websocket, this should never happen."),
+      _ => new PSRemotingTransportException(
+        "Websocket Unexpectedly Closed, see InnerException for details",
+        new WebSocketException(
+          WebSocketError.ConnectionClosedPrematurely,
+          $"Server sent a unexpected close status of {receiveResult.CloseStatus} with reason {receiveResult.CloseStatusDescription}"
+        )
+      )
+    };
   }
 
   public override void CreateAsync()
